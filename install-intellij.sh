8fb67752c3d171f36d93248b4bb3801ceaeba032
Brett Napier <>
2019-04-16 20:10:36 -0400

    Add logging for IntelliJ IDEA install

--- install-intellij.sh
+++ install-intellij.sh
@@ -2,4 +2,6 @@
 # wget https://download-cf.jetbrains.com/idea/ideaIU-2019.1.tar.gz
 # tar xvzf ideaIU-2019.1.tar.gz
 # cd idea-IU-191.6183.87/
+echo -e "\e[31m !---------- IntelliJ Installing ----------! \e[0m"
 snap install intellij-idea-ultimate --classic
+echo -e "\e[32m !---------- IntelliJ Installed ----------! \e[0m"
