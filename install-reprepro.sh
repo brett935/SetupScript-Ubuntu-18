d16f36c6973454f39def61bceff43dd470091841
Brett Napier <>
2019-06-13 14:45:23 -0400

    Add RepRepro to installer

---
+++ install-reprepro.sh
@@ -0,0 +1,4 @@
+# install reprepro
+echo -e "\e[31m !---------- reprepro Installing ----------! \e[0m"
+sudo apt -y install reprepro
+echo -e "\e[32m !---------- reprepro Installed ----------! \e[0m"
