91b1c7c113a87c78fea4cd3a5104dc36c5d57d44
Brett Napier <>
2019-06-13 14:44:33 -0400

    Add bash aliases

--- configure-bashrc.sh
+++ configure-bashrc.sh
@@ -9,8 +9,18 @@ echo "alias c='cd ~/Code'" >> ~/.bashrc
 echo "alias n='nano -Bu' # backup file and allow undos " >> ~/.bashrc
 echo "alias bn='play -n synth brownnoise'" >> ~/.bashrc
 echo "alias a='npm start'" >> ~/.bashrc
-echo "alias g='google-chrome --remote-debugging-port=9222'" >> ~/.bashrc
+echo "alias gc='google-chrome --remote-debugging-port=9222'" >> ~/.bashrc
+echo "alias g='git'" >> ~/.bashrc
+echo "alias gs='git status'" >> ~/.bashrc
+echo "alias gp='git push'" >> ~/.bashrc
+echo "alias gb='git branch'" >> ~/.bashrc
+echo "alias gr='git rebase'" >> ~/.bashrc
+echo "alias ns='npm start'" >> ~/.bashrc
+
 echo "alias u='cd ..'"
+cat << EOF >> ~/.bashrc
+alias t='npm run test && npm run lint && echo -e "\e[32m !---------- Test and linting passed!  ----------! \e[0m"'
+EOF
 source ~/.bashrc
 
 echo -e "\e[32m !---------- Configured ~/.bashrc ----------! \e[0m"
